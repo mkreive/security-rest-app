06ef6632513438501946b05a2dc3d791c98736aa
mkreive <mkreive@gmail.com>
2023-07-11 15:32:07 +0300

    users and paswords added to controller for reference

--- src/main/java/lt/monikos/security/controller/AppController.java
+++ src/main/java/lt/monikos/security/controller/AppController.java
@@ -8,16 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class AppController {
 
+    // anna, bob, carol pw:test123
     @GetMapping("/users")
     public String users() {
         return "Hello USERS";
     }
 
+
+    //  bob, carol pw:test123
     @GetMapping("/managers")
     public String managers() {
         return "Hello MANAGERS";
     }
 
+    //  carol pw:test123
     @GetMapping("/admins")
     public String admins() {
         return "Hello ADMINS";
