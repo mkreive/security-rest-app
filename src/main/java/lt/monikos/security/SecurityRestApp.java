faa90a0d93f5f93b52703cdef8ec5781e14e2813
mkreive <mkreive@gmail.com>
2023-07-11 15:09:59 +0300

    initial

---
+++ src/main/java/lt/monikos/security/SecurityRestApp.java
@@ -0,0 +1,13 @@
+package lt.monikos.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityRestApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityRestApp.class, args);
+	}
+
+}
---
+++ src/test/java/lt/monikos/security/SecurityRestAppTests.java
@@ -0,0 +1,13 @@
+package lt.monikos.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityRestAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
